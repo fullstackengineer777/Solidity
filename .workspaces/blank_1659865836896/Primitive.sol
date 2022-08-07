13c9db9b60037ae5d5ebba5c18cb861d30c3d77b
Timoteus K <softdeveng9211@outlook.com>
2022-08-07 16:27:42 +0300

    Solidity By Example

---
+++ .workspaces/blank_1659865836896/Primitive.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Primitives{
+    
+    bool public boo = true;
+
+    /*
+    Negative numbers are allowed for int types.
+    Like uint, different ranges are available from int8 to int256
+    
+    int256 ranges from -2 ** 255 to 2 ** 255 - 1
+    int128 ranges from -2 ** 127 to 2 ** 127 - 1
+    */
+    uint8 public u8 = 1;
+    uint public u256 = 456;
+    uint public u = 123;
+
+    //minimum and maximum of int
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    /*
+    In Solidity, the data type byte represent a sequence of bytes. 
+    Solidity presents two type of bytes types :
+
+     - fixed-sized byte arrays
+     - dynamically-sized byte arrays.
+     
+     The term bytes in Solidity represents a dynamic array of bytes. 
+     It’s a shorthand for byte[] .
+    */
+    bytes1 a = 0xb5; //  [10110101]
+    bytes1 b = 0x56; //  [01010110]
+
+    bool public defaultBoo;    //false
+    uint public defaultUint;   //0
+    int public defaultInt;     //0
+    address public defaultAddr;//0x0000000000000000000000000000000000000000
+
+
+}
