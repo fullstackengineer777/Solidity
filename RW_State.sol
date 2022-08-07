406f302befe80ed3cf916c474703e6ad26e48c26
Timoteus K <softdeveng9211@outlook.com>
2022-08-07 17:12:46 +0300

    Solidity By Examples

---
+++ RW_State.sol
@@ -0,0 +1,26 @@
+// To write or update a state variable you need to send a transaction.
+// On the other hand, you can read state variables,
+// for free, without any transaction fee.
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract SimpleStorage{
+
+    //State variable to store a number
+    uint public num;
+
+    // You need to send a transaction to write to a state variable.
+    function set(uint _num) public {
+        num = _num;
+    }
+
+    // You can read from a state variable without sending a transaction.
+    function get() public view returns (uint) {
+        return num;
+    }
+
+}
+
+
